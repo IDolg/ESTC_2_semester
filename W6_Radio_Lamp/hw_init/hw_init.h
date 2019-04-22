ffbf976f0629f5d0a362b894d577b434b3ef1ec5
igor <igor1@asus.Dlink>
2019-04-22 23:42:38 +0300

    w6 added

---
+++ W6_Radio_Lamp/hw_init/hw_init.h
@@ -0,0 +1,9 @@
+#include "../../../include/zb_types.h"
+
+
+
+void InitButtonInterruptions(void);
+void InitButtons(void);
+void init_LED(void);
+void blink(void); 
+
---
+++ W6_Radio_Lamp/light_cmd/light_cmd.c
@@ -0,0 +1,68 @@
+#ifndef LIGHT_CMD
+#define LIGHT_CMD 1
+
+#include "zb_common.h"
+#include "zb_scheduler.h"
+#include "zb_bufpool.h"
+#include "zb_nwk.h"
+#include "zb_aps.h"
+#include "zb_zdo.h"
+#include "stm32f4xx.h"
+#include "../lib_set_color/SetColor.h"
+
+#include "light_cmd.h"
+
+void send_packet(zb_uint8_t param, zb_uint8_t *data, int len, zb_uint16_t addr)
+{
+    zb_buf_t *buf = ZB_BUF_FROM_REF(param);  
+    zb_uint8_t *ptr = NULL;
+    ZB_BUF_INITIAL_ALLOC(buf, len, ptr);
+    memcpy(ptr,data,len);
+    
+    zb_apsde_data_req_t *req = ZB_GET_BUF_TAIL(buf, sizeof(zb_apsde_data_req_t));
+    req->dst_addr.addr_short = addr;
+    req->addr_mode = ZB_APS_ADDR_MODE_16_ENDP_PRESENT;
+    req->tx_options = ZB_APSDE_TX_OPT_ACK_TX;
+    req->radius = 1;
+    req->profileid = 2;
+    req->src_endpoint = 10;
+    req->dst_endpoint = 10;
+    buf->u.hdr.handle = 0x11;
+    
+    ZB_SCHEDULE_CALLBACK(zb_apsde_data_request, param);
+}; 
+
+void send_cmd_change_color(zb_uint8_t param)
+{
+    zb_buf_t *buf = ZB_BUF_FROM_REF(param); 
+    zb_uint16_t *user_parameter = ZB_GET_BUF_TAIL(buf, sizeof(zb_uint16_t));
+    zb_uint16_t addr = *user_parameter;
+    
+    zb_uint8_t data[1];
+    data[0] = CMD_CHANGE_COLOR;
+    send_packet(param, data, 1, addr);
+ };
+
+void send_cmd_toggle(zb_uint8_t param)
+{
+    zb_buf_t *buf = ZB_BUF_FROM_REF(param); 
+    zb_uint16_t *user_parameter = ZB_GET_BUF_TAIL(buf, sizeof(zb_uint16_t));
+    zb_uint16_t addr = *user_parameter;
+    
+    zb_uint8_t data[1];
+    data[0] = CMD_TOGGLE;
+    send_packet(param, data, 1, addr);
+ }; 
+
+ void send_cmd_step_up(zb_uint8_t param)
+{
+    zb_buf_t *buf = ZB_BUF_FROM_REF(param); 
+    zb_uint16_t *user_parameter = ZB_GET_BUF_TAIL(buf, sizeof(zb_uint16_t));
+    zb_uint16_t addr = *user_parameter;
+    
+    zb_uint8_t data[1];
+    data[0] = CMD_STEP_UP;
+    send_packet(param, data, 1, addr);
+ }; 
+ 
+#endif
